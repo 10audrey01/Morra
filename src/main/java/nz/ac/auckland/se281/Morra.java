a8f9a3760a0a18b893f55a5f0f0ee42c2322816c
audrey <ayou332@aucklanduni.ac.nz>
2023-05-06 19:14:20 +1200

    Implemented play method with all requirements

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -3,12 +3,50 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
+  int numberOfRounds;
+  String playerName;
 
   public Morra() {}
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    numberOfRounds = 1;
+    playerName = options[0];
+  }
 
-  public void play() {}
+  public void play() {
+    String input;
+    String[] inputArray;
+    boolean isInteger;
+    int fingers;
+    int sum;
+    boolean inputValid = false;
+
+    MessageCli.START_ROUND.printMessage(String.valueOf(numberOfRounds));
+
+    while (!inputValid) {
+      MessageCli.ASK_INPUT.printMessage();
+      input = Utils.scanner.nextLine();
+      inputArray = input.split(" ");
+      isInteger = Utils.isInteger(inputArray[0]) && Utils.isInteger(inputArray[1]);
+      if (isInteger) {
+        fingers = Integer.parseInt(inputArray[0]);
+        sum = Integer.parseInt(inputArray[1]);
+      } else {
+        fingers = 0;
+        sum = 0;
+      }
+      if ((inputArray.length == 2)
+          && (isInteger)
+          && (fingers >= 1 && fingers <= 5 && sum >= 1 && sum <= 10)) {
+        inputValid = true;
+        MessageCli.PRINT_INFO_HAND.printMessage(playerName, inputArray[0], inputArray[1]);
+        numberOfRounds++;
+      } else {
+        MessageCli.INVALID_INPUT.printMessage();
+      }
+    }
+  }
 
   public void showStats() {}
 }
