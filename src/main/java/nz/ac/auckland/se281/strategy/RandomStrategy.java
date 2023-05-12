12656240a346618e89661b1edca77da063bfcdac
audrey <ayou332@aucklanduni.ac.nz>
2023-05-12 20:31:50 +1200

    More formatting

--- src/main/java/nz/ac/auckland/se281/strategy/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/strategy/RandomStrategy.java
@@ -4,7 +4,6 @@ import nz.ac.auckland.se281.FingersAndSum;
 import nz.ac.auckland.se281.Utils;
 
 public class RandomStrategy extends Strategy {
-
   @Override
   public FingersAndSum selectFingersAndSum() {
     int fingers = Utils.getRandomNumber(1, 5);
